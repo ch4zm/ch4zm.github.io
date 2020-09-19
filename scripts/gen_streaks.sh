770551c1e43f4c099b564ad4ca9ef163476f1c07
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-09-19 11:58:02 -0700

    make master win/lose streaks page

--- scripts/gen_streaks.sh
+++ scripts/gen_streaks.sh
@@ -19,17 +19,34 @@ source $ACTIVATE_SCRIPT
 ########################## blaseball-streak-finder #######################################
 
 
+# As we go, we also want to assemble master winning streak/losing streak docs
+WMASTER="$ROOT_DIR/../docs/wstreaks.md"
+LMASTER="$ROOT_DIR/../docs/lstreaks.md"
+
+cat /dev/null > $WMASTER
+echo "# Blaseball Winning Streaks" >> $WMASTER
+echo "" >> $WMASTER
+echo "# Blaseball Losing Streaks" >> $LMASTER
+echo "" >> $LMASTER
+
 
 echo "############################################"
 echo "# Winning Streaks:"
 
 # All-time
 echo "Creating all-time winning streaks page..."
-OUT=$ROOT_DIR/../docs/wstreaks_alltime.md
+fname="wstreaks_alltime.md"
+OUT=$ROOT_DIR/../docs/$fname
 echo "Working on page: $(basename $OUT)"
 
+title="All-Time Blaseball Winning Streaks"
+echo "## All Time" >> $WMASTER
+echo "" >> $WMASTER
+echo "[$title]($fname)" >> $WMASTER
+echo "" >> $WMASTER
+
 cat /dev/null > $OUT
-echo "# All-Time Blaseball Winning Streaks" >> $OUT
+echo "# $title" >> $OUT
 echo "(Through season $LASTSEASON)" >> $OUT
 echo "## Summary" >> $OUT
 $STREAK_FINDER --winning --short --markdown --min 8 | $ROOT_DIR/split_tables.py >> $OUT
@@ -41,12 +58,19 @@ echo "Done."
 
 # By Season
 echo "Creating season-by-season winning streaks page..."
+echo "## By Season" >> $WMASTER
+echo "" >> $WMASTER
 for i in $(seq 1 $LASTSEASON); do 
-    OUT=$ROOT_DIR/../docs/wstreaks_season$i.md
+    fname="wstreaks_season$i.md"
+    OUT="$ROOT_DIR/../docs/$fname"
     echo "Working on season $i: $(basename $OUT)"
 
+    title="Season $i Winning Streaks"
+    echo "* [$title]($fname)" >> $WMASTER
+    echo "" >> $WMASTER
+
     cat /dev/null > $OUT
-    echo "# Season $i Winning Streaks" >> $OUT
+    echo "# $title" >> $OUT
     echo "## Summary" >> $OUT
     $STREAK_FINDER --winning --season $i --short --markdown --min 7 | $ROOT_DIR/split_tables.py >> $OUT
     echo "" >> $OUT
@@ -58,13 +82,20 @@ echo "Done."
 
 # By Team
 echo "Creating team-by-team winning streaks page..."
+echo "## By Team" >> $WMASTER
+echo "" >> $WMASTER
 cat $ROOT_DIR/../list_of_teams | sort | while read team; do
     lower_team=$(echo $team | awk '{print tolower($0)}' | sed 's/ /_/g' )
-    OUT=$ROOT_DIR/../docs/wstreaks_team_${lower_team}.md
+    fname="wstreaks_team_${lower_team}.md"
+    OUT="$ROOT_DIR/../docs/$fname"
     echo "Working on team $team: $(basename $OUT)"
 
+    title="$team Winning Streaks"
+    echo "* [$title]($fname)" >> $WMASTER
+    echo "" >> $WMASTER
+
     cat /dev/null > $OUT
-    echo "# $team Winning Streaks" >> $OUT
+    echo "# $title" >> $OUT
     echo "(Through season $LASTSEASON)" >> $OUT
     echo "## Summary" >> $OUT
     $STREAK_FINDER --winning --team "$team" --short --markdown --min 4 | $ROOT_DIR/split_tables.py >> $OUT
@@ -81,11 +112,18 @@ echo "# Losing Streaks:"
 
 # All-time
 echo "Creating all-time losing streaks page..."
-OUT=$ROOT_DIR/../docs/lstreaks_alltime.md
+fname="lstreaks_alltime.md"
+OUT="$ROOT_DIR/../docs/$fname"
 echo "Working on page: $(basename $OUT)"
 
+title="All-Time Blaseball Losing Streaks"
+echo "## All Time" >> $LMASTER
+echo "" >> $LMASTER
+echo "[$title]($fname)" >> $LMASTER
+echo "" >> $LMASTER
+
 cat /dev/null > $OUT
-echo "# All-Time Blaseball Losing Streaks" >> $OUT
+echo "# $title" >> $OUT
 echo "(Through season $LASTSEASON)" >> $OUT
 echo "## Summary" >> $OUT
 $STREAK_FINDER --losing --short --markdown --min 8 | $ROOT_DIR/split_tables.py >> $OUT
@@ -97,12 +135,19 @@ echo "Done."
 
 # By Season
 echo "Creating season-by-season losing streaks page..."
+echo "## By Season" >> $LMASTER
+echo "" >> $LMASTER
 for i in $(seq 1 $LASTSEASON); do 
-    OUT=$ROOT_DIR/../docs/lstreaks_season$i.md
+    fname="lstreaks_season$i.md"
+    OUT="$ROOT_DIR/../docs/$fname"
     echo "Working on season $i: $(basename $OUT)"
 
+    title="Season $i Losing Streaks"
+    echo "* [$title]($fname)" >> $LMASTER
+    echo "" >> $LMASTER
+
     cat /dev/null > $OUT
-    echo "# Season $i Losing Streaks" >> $OUT
+    echo "# $title" >> $OUT
     echo "## Summary" >> $OUT
     $STREAK_FINDER --losing --season $i --short --markdown --min 7 | $ROOT_DIR/split_tables.py >> $OUT
     echo "" >> $OUT
@@ -114,13 +159,19 @@ echo "Done."
 
 # By Team
 echo "Creating team-by-team losing streaks page..."
+echo "## By Team" >> $LMASTER
+echo "" >> $LMASTER
 cat $ROOT_DIR/../list_of_teams | sort | while read team; do
     lower_team=$(echo $team | awk '{print tolower($0)}' | sed 's/ /_/g' )
-    OUT=$ROOT_DIR/../docs/lstreaks_team_${lower_team}.md
+    fname="lstreaks_team_${lower_team}.md"
+    OUT="$ROOT_DIR/../docs/$fname"
     echo "Working on team $team: $(basename $OUT)"
 
+    title="$team Losing Streaks"
+    echo "* [$title]($fname)" >> $LMASTER
+    echo "" >> $LMASTER
+
     cat /dev/null > $OUT
-    echo "# $team Losing Streaks" >> $OUT
     echo "(Through season $LASTSEASON)" >> $OUT
     echo "## Summary" >> $OUT
     $STREAK_FINDER --losing --team "$team" --short --markdown --min 4 | $ROOT_DIR/split_tables.py >> $OUT
@@ -130,6 +181,4 @@ cat $ROOT_DIR/../list_of_teams | sort | while read team; do
     echo "" >> $OUT
 done
 echo "Done."
-
-
 
